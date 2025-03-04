1eeabac65f56ea584b8d1bfd042d6f87d97eadb9
Daniela <nam991019@gmail.com>
2025-03-04 15:31:47 -0500

    inicio commit(class)

---
+++ parcial.js
@@ -0,0 +1,11 @@
+// se defina la clase "pedidos"
+class pedidos{
+     //Utilizo el metodo constructor para crear e iniciar un objeto creado.
+    constructor (Nombre, ID, fecha, Prioridad, Descripcion){
+        this.Nombre = Nombre;
+        this.ID = ID;
+        this.fecha;
+        this.Prioridad = Prioridad;
+        this.Descripcion = Descripcion;
+    }
+}
