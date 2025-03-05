431bf88a98a0d52665a5e89584cfc447488be492
Daniela <nam991019@gmail.com>
2025-03-04 20:46:38 -0500

    class pedido sistema

--- parcial.js
+++ parcial.js
@@ -9,3 +9,43 @@ class pedidos{
         this.Descripcion = Descripcion;
     }
 }
+
+//Ahora se crea el sistema de pedidos
+class sistemapedido {
+    constructor (){
+        this.pedidos = [];
+    }
+
+    //metodo para registrar un nuevo pedido en el sitema
+    registrarpedido(pedido){
+        this.pedidos.push(pedido);
+    }
+
+    //consultar el estado de un pedido 
+    consultarPedido(){
+        return this.pedidos;
+    }
+
+    // Buscar el pedido por ID
+    buscarpedidoid(ID) {
+        return this.pedidos.find(pedido => pedido.ID === ID);
+    }
+
+    //ordenar pedidos por fecha o prioridad
+    pedidoPorFecha(criterio){
+        if (criterio === `fecha`) {
+            return this.pedidos.sort((a, b) => new Date(a.fecha) - new Date (b.fecha));
+        } else if(criterio === `prioridad`){
+           //
+            return this.pedidos.sort((a, b) =>{
+                    const prioridades = {alta: 3, media: 2, baja: 1,};
+                    return prioridades [a.Prioridad] - [b.Prioridad];
+            }); 
+        }
+    }
+
+    //eliminar pedido por ID
+    eliminarPorId(ID){
+        this.pedidos.filter(pedido => pedido.ID !== ID);
+    }
+}
